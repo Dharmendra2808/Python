db73164053bb31eb5841d78ce7381dd94195ecfb
Dharmendra Kumar <161357487+Dharmendra2808@users.noreply.github.com>
2025-10-29 11:52:50 +0530

    Practice of python within if else statements

---
+++ barexercise.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age : "))
+
+if age>60:
+    print("Sorry u can't have alcohol")
+elif age>=18:
+    print("Yeah sure , Which drink u would like ?")
+else:
+    print("Sorry U are too young to have alcohol")
