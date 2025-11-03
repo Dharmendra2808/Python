8eaea2cb15f5460c908be58f6c12c79cf94e103e
Dharmendra Kumar <161357487+Dharmendra2808@users.noreply.github.com>
2025-11-03 16:34:51 +0530

    Small Practice with if, elif and else statements

---
+++ Rock_paper_game.py
@@ -0,0 +1,23 @@
+import random
+print("Let's Play Bro")
+user=input("Enter your input in game ")
+computer_choice=random.choice(["Rock","Scissor","Paper"])
+print(user,computer_choice)
+
+if user==computer_choice:
+    print("Your choice and sytem choice both are same ,match ties")
+elif user=="Rock":
+    if computer_choice=="scissor":
+        print("Rock Break scissor ,User wins")
+    else:
+        print("Computer wins")
+elif user=="Scissor":
+    if computer_choice=="paper":
+        print("Scissor cut paper ,User wins")
+    else:
+        print("Computer wins")
+elif user=="Paper":
+    if computer_choice=="Rock":
+        print("Paper Fold rock ,User wins")
+    else:
+        print("Computer wins")
